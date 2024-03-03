71d7b1b0cab2750514d392afb5002bf031cec32e
Racheal Appiah-kubi <coderacheal@gmail.com>
2024-03-03 22:26:52 +0000

    Complete predict, data and history page

--- pages/03_Predict.py
+++ pages/03_Predict.py
@@ -2,6 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 import os
+import datetime
 
 
 st.set_page_config(
@@ -10,28 +11,25 @@ st.set_page_config(
     layout='wide'
 )
 
-if not os.path.exists("./data"):
-    os.makedirs("./data")
 
-
-st.cache_resource(show_spinner='Model Loading')
+@st.cache_resource(show_spinner='Models Loading')
 def load_forest_pipeline():
     pipeline = joblib.load('./models/forest_pipeline.joblib')
     return pipeline
 
 
-st.cache_resource(show_spinner='Model Loading')
+@st.cache_resource(show_spinner='Models Loading')
 def load_scv_pipeline():
     pipeline = joblib.load('./models/svc_pipeline.joblib')
     return pipeline
 
 
-st.cache_resource()
 def select_model():
     col1, col2 = st.columns(2)
 
     with col1:
-        st.selectbox('Select a Model', options=['Random Forest', 'SVC'], key='selected_model')
+        st.selectbox('Select a Model', options=[
+                     'Random Forest', 'SVC'], key='selected_model')
     with col2:
         pass
 
@@ -43,6 +41,16 @@ def select_model():
     encoder = joblib.load('./models/encoder.joblib')
 
     return pipeline, encoder
+
+
+if 'prediction' not in st.session_state:
+    st.session_state['prediction'] = None
+if 'probability' not in st.session_state:
+    st.session_state['probability'] = None
+
+
+if not os.path.exists('./data'):
+    os.mkdir("./data")
 
 
 def make_prediction(pipeline, encoder):
@@ -60,72 +68,95 @@ def make_prediction(pipeline, encoder):
     yearsatcompany = st.session_state['yearsatcompany']
 
     columns = ['Age', 'Department', 'DistanceFromHome', 'Education', 'EducationField',
-       'EnvironmentSatisfaction', 'JobSatisfaction', 'MaritalStatus',
-        'MonthlyIncome', 'NumCompaniesWorked', 'WorkLifeBalance',
-        'YearsAtCompany']
-    
+               'EnvironmentSatisfaction', 'JobSatisfaction', 'MaritalStatus',
+               'MonthlyIncome', 'NumCompaniesWorked', 'WorkLifeBalance',
+               'YearsAtCompany']
+
     data = [[age, department, distancefromhome, education, educational_field, environmental_satisfaction, job_statisfaction, marital_status, monthly_income,
              numofcompaniesworked, worklifebalance,  yearsatcompany]]
-    
-    #create a dataframe
+
+    # create a dataframe
     df = pd.DataFrame(data, columns=columns)
 
-    #Make prediction
+    df['Prediciton Time'] = datetime.date.today()
+    df['Model Used'] = st.session_state['selected_model']
+
+    df.to_csv('./data/history.csv', mode='a', header=not os.path.exists('./data/history.csv'), index=False)
+
+    # Make prediction
     pred = pipeline.predict(df)
-    prediction  = int(pred[0])
-    prediction = encoder.inverse_transform([prediction])
+    pred = int(pred[0])
+    prediction = encoder.inverse_transform([pred])
 
-    #Get probabilities
-    probability = pipeline.predict_proba(df)
+    # Get probabilities
+    probability = pipeline.predict_proba(pred)
 
-    #Updating state
+    # Updating state
     st.session_state['prediction'] = prediction
     st.session_state['probability'] = probability
 
-    return prediction
+    return prediction, probability
+
+
 
 
 def display_form():
 
-    with st.spinner('Models Loading ...'):
-        pipeline, encoder = select_model()
+    pipeline, encoder = select_model()
 
     with st.form('input-feature'):
         col1, col2, col3 = st.columns(3)
 
         with col1:
             st.write('### Personal Information')
-            st.number_input('Enter age', min_value=18, max_value=60, step=1, key='age')
-            st.number_input('Distance from Home', min_value=1, max_value=35, key='distancefromhome')
-            st.number_input('Monthly Income', min_value=1000, step=100, key='monthly_income')
-            st.selectbox('Marital Status', ['Single', 'Married', 'Divorced'], key='marital_status')
-            
+            st.number_input('Enter age', min_value=18,
+                            max_value=60, step=1, key='age')
+            st.number_input('Distance from Home', min_value=1,
+                            max_value=35, key='distancefromhome')
+            st.number_input('Monthly Income', min_value=1000,
+                            step=100, key='monthly_income')
+            st.selectbox('Marital Status', [
+                         'Single', 'Married', 'Divorced'], key='marital_status')
 
         with col2:
             st.write('### Work Information')
-            st.selectbox('Enter department', options=['Sales', 'Research & Development', 'Human Resources'], key='department')
+            st.selectbox('Enter department', options=[
+                         'Sales', 'Research & Development', 'Human Resources'], key='department')
             st.selectbox('Enter Educational Field', options=['Life Sciences', 'Other', 'Medical', 'Marketing',
-            'Technical Degree', 'Human Resources'], key='educational_field')
-            st.number_input('Education', min_value=1, max_value=5, key='education')
-            st.number_input("Years at company", min_value=1, key='yearsatcompany')
+                                                             'Technical Degree', 'Human Resources'], key='educational_field')
+            st.number_input('Education', min_value=1,
+                            max_value=5, key='education')
+            st.number_input("Years at company", min_value=1,
+                            key='yearsatcompany')
 
         with col3:
             st.write('### Satisfaction')
-            st.number_input('Job Satisfaction', min_value=1, max_value=4, key='job_statisfaction')
-            st.number_input('Environment Satisfaction', min_value=1, max_value=4, key='environmental_satisfaction')
-            st.number_input('Work-Life Balance', min_value=1, max_value=4, key='worklifebalance')
-            st.number_input('Num of Companies Worked At', min_value=1, max_value=4, key='numofcompaniesworked')
+            st.number_input('Job Satisfaction', min_value=1,
+                            max_value=4, key='job_statisfaction')
+            st.number_input('Environment Satisfaction', min_value=1,
+                            max_value=4, key='environmental_satisfaction')
+            st.number_input('Work-Life Balance', min_value=1,
+                            max_value=4, key='worklifebalance')
+            st.number_input('Num of Companies Worked At', min_value=1,
+                            max_value=4, key='numofcompaniesworked')
 
-        st.form_submit_button('Submit', on_click=make_prediction, kwargs=dict(pipeline=pipeline, encoder=encoder))
+        st.form_submit_button('Submit', on_click=make_prediction, kwargs=dict(
+            pipeline=pipeline, encoder=encoder))
 
 
 if __name__ == "__main__":
     st.title("Make a Prediction")
     display_form()
 
-
     prediction = st.session_state['prediction']
     probability = st.session_state['probability']
 
+    if not prediction:
+        st.markdown("### Predictions will show here")
+    elif prediction == "Yes":
+        probability_of_yes = probability[0][1] * 100
+        st.markdown(f"### The employee will leave the company with a probability of {round(probability_of_yes, 2)}%")
+    else:
+        probability_of_no = probability[0][0] * 100
+        st.markdown(f"### Employee will not leave with a probablity of {round(probability_of_no, 2)}%")
 
-    st.write(st.session_state)
--- pages/04_History.py
+++ pages/04_History.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title='History',
@@ -7,3 +8,17 @@ st.set_page_config(
 )
 
 st.title('Prediciton History')
+
+
+def show_historic_predictions():
+    
+    csv_path = "./data/history.csv"
+    df = pd.read_csv(csv_path)
+
+    return df
+
+
+
+if __name__ == "__main__":
+    df = show_historic_predictions()
+    st.dataframe(df)
