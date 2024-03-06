71d3f9757db0ffd7878baa9caf94ba2e615abad8
Racheal Appiah-kubi <coderacheal@gmail.com>
2024-03-06 08:42:32 +0000

    Update setup.sh - Remove sudo

--- setup.sh
+++ setup.sh
@@ -1,29 +1,18 @@
-curl https://packages.microsoft.com/keys/microsoft.asc | sudo tee /etc/apt/trusted.gpg.d/microsoft.asc
-
-#Download appropriate package for the OS version
-#Choose only ONE of the following, corresponding to your OS version
-
-#Debian 9
-# curl https://packages.microsoft.com/config/debian/9/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
-
-#Debian 10
-# curl https://packages.microsoft.com/config/debian/10/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
-
-#Debian 11
-# curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/keys/microsoft.asc | tee /etc/apt/trusted.gpg.d/microsoft.asc
 
 #Debian 12
 curl https://packages.microsoft.com/config/debian/12/prod.list | tee /etc/apt/sources.list.d/mssql-release.list
 
-# apt-get update
-# apt-get install sudo
 apt-get update
 ACCEPT_EULA=Y apt-get install -y msodbcsql17
+
 # optional: for bcp and sqlcmd
 ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
+
 # optional: for unixODBC development headers
 apt-get install -y unixodbc-dev
+
 # optional: kerberos library for debian-slim distributions
 apt-get install -y libgssapi-krb5-2
