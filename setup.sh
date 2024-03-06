fa62a3c921a094ecaf5a51b725cab86d8bb13b40
Racheal Appiah-kubi <coderacheal@gmail.com>
2024-03-06 08:28:52 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,15 +15,15 @@ curl https://packages.microsoft.com/config/debian/11/prod.list | sudo tee /etc/a
 #Debian 12
 curl https://packages.microsoft.com/config/debian/12/prod.list | sudo tee /etc/apt/sources.list.d/mssql-release.list
 
+# apt-get update
+# apt-get install sudo
 apt-get update
-apt-get install sudo
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql17
+ACCEPT_EULA=Y apt-get install -y msodbcsql17
 # optional: for bcp and sqlcmd
-sudo ACCEPT_EULA=Y apt-get install -y mssql-tools
+ACCEPT_EULA=Y apt-get install -y mssql-tools
 echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev
 # optional: kerberos library for debian-slim distributions
-sudo apt-get install -y libgssapi-krb5-2
+apt-get install -y libgssapi-krb5-2
